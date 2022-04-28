7ca846b09cbecd9c7265c429e368e48d7d759ead
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-04-28 19:30:23 +0530

    fisrt

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import "./footer.css"
+
+function Footer() {
+  
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © 2022</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Sumit Thakur</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/SamSumit007"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/sumit-thakur-23858516a/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/Pages/Projects.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCards from "../Components/ProjectCards/ProjectCards";
+
+function Projects() {
+  return (
+    <Container id="projects" fluid className="project-section">
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Col md={6} className="project-card" >
+            <ProjectCards
+              imgPath="/images/supermarket.png"
+              title="blinkit"
+              description="Blinkit is an Indian instant delivery service.to order groceries and essentials online. Blinkit's employees then secure the items from their warehouse and deliver the items to the consumer within 10 minutes "
+              tech="Tech-Stacks"
+              techD="Html | Javascript | Css | Advanced JavaScript "
+              link="https://blinkit.netlify.app/"
+              a="https://github.com/Imaryan08/blinkit"
+            />
+          </Col>
+          <Col md={6} className="project-card">
+            <ProjectCards
+              imgPath="/images/unsplash.jpeg"
+              title="Unsplash"
+              description="Clone of a Website called Unsplash which lets its user to download some free and premium images."
+              tech="Tech-Stacks"
+              techD="Html | Javascript | Css "
+              link="https://sumit-unsplash-ssprm902.netlify.app/"
+              a="https://github.com/mayank8887/Construct_Unsplash"
+            />
+          </Col>
+
+         
+
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Projects;
